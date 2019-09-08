9ade014563fda1d723ba68d000092b9abbfabb30
Dan Quan <dan@quan.io>
2019-09-08 12:01:22 -0700

    Show upstream

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,7 @@ alias kctx='kubectx'
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
