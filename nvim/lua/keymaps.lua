edd375cfb04917ccd502b4a2f49096d50f8e3745
Dan Quan <dan@quan.io>
2021-07-05 11:01:54 -0700

    Use lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,13 +2,52 @@ local map = vim.api.nvim_set_keymap
 local function noremap(mode, keymap, command)
   map(mode, keymap, command, { noremap = true })
 end
+local cmd = vim.cmd
 
 noremap('v', '.', ':norm.<CR>')
 map('', '<C-s>', '<esc>:w<CR>', {})
 map('i', '<C-s>', '<esc>:w<CR>', {})
+map('n', '<leader>v', ':e ~/dotfiles/nvim/<CR>', {})
+map('n', '<leader>nh', ':nohls<CR>', {})
 
+map('n', '<leader>ut', ':UndotreeToggle<cr>', {})
+map('n', '<leader>at', ':ALEToggle<cr>', {})
+map('', '<leader>nt', ':NERDTreeToggle<cr>', {})
+map('', '<leader>nf', ':NERDTreeFind<cr>', {})
+map('', '<leader>nf', ':NERDTree<cr>', {})
+
+noremap('n', '<leader>gt', ':A<CR>')
+
+-- vim test
 map('n', '<leader>tn', ':TestNearest<CR>', {})
 map('n', '<leader>tf', ':TestFile<CR>', {})
 map('n', '<leader>tt', ':TestSuite<CR>', {})
 map('n', '<leader>tl', ':TestLast<CR>', {})
 map('n', '<leader>tv', ':TestVisit<CR>', {})
+
+-- lspsaga
+noremap('n', 'gh', ':Lspsaga lsp_finder<CR>')
+noremap('n', '<leader>ca', ':Lspsaga code_action<CR>')
+noremap('v', '<leader>ca', '<C-U>Lspsaga range_code_action<CR>')
+noremap('n', 'K', ':Lspsaga hover_doc<CR>')
+noremap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
+noremap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
+noremap('n', 'gs', ':Lspsaga signature_help<CR>')
+noremap('n', 'gr', ':Lspsaga rename<CR>')
+noremap('n', 'gd', ':Lspsaga preview_definition<CR>')
+noremap('n', '<leader>cd', ':Lspsaga show_line_diagnostics<CR>')
+noremap('n', '<leader>cc', "<cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>")
+noremap('n', '[e', ':Lspsaga diagnostic_jump_next<CR>')
+noremap('n', ']e', ':Lspsaga diagnostic_jump_prev<CR>')
+
+-- telescope
+map('', '<leader>b', '<cmd>Telescope buffers<cr>', {})
+map('', '<C-P>', ":lua require('telescope.builtin').find_files{ find_command = {'rg', '--files', '--hidden', '--iglob', '!.git'} }<CR>", {})
+map('', '<leader>rg', '<cmd>Telescope live_grep<cr>', {})
+
+-- compe
+cmd("inoremap <silent><expr> <C-x> compe#complete()")
+cmd("inoremap <silent><expr> <CR>      compe#confirm('<CR>')")
+cmd("inoremap <silent><expr> <C-e>     compe#close('<C-e>')")
+cmd("inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })")
+cmd("inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })")
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,7 @@
 local options = vim.o
 local global = vim.g
 local cmd = vim.cmd
+local set_global=vim.api.nvim_set_var
 
 options.relativenumber=true
 options.number=true
@@ -37,3 +38,18 @@ global.nowritebackup=true
 cmd("syntax on")
 cmd("colorscheme gruvbox")
 cmd("set undodir=\"~/.vim/undodir\"") -- lua options do not expand home dir
+cmd('let test#strategy = "vimux"')
+cmd('autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o')
+cmd('autocmd BufWritePost plugins.lua PackerCompile')
+
+set_global('ale_enabled', 1)
+set_global('ale_lint_on_text_changed', 'normal')
+set_global('ale_lint_on_insert_leave', 1)
+
+local projectionist_heuristics = {}
+local go_heuristics = {}
+go_heuristics['*.go'] = { alternate = '{}_test.go', type = 'source' }
+go_heuristics['*_test.go'] = { alternate = '{}.go', type = 'test' }
+projectionist_heuristics["*.go"] = go_heuristics
+
+set_global('projectionist_heuristics', projectionist_heuristics)
