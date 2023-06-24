2bb4423a722218eb023be74ef427eb016ab5896e
Dan Quan <dan@quan.io>
2023-06-24 09:26:33 -0700

    Update dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+
+alias gd="git diff"
+alias gst="git status"
+
+alias ll="ls -lah"
+alias l="ls -l"
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
