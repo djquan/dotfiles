44d7943c9d1defdc4690b360d73dcb50b3de3cb1
Dan Quan <dan@quan.io>
2025-06-26 06:27:46 -0700

    Add ghostty

--- .wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
-
-
-config.color_scheme = 'Monokai (base16)'
-config.default_prog = { '/bin/zsh', '-l', '-c', 'tmux new -A -s dev' }
-
-return config
